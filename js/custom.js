5f202b3d57b433855a035967d421c3e99949b7c7
Rémy Jacquet <jacquet.remy@yahoo.fr>
2021-11-24 11:25:29 +0100

    bar de piratage

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+$("#piratage_start").click(function() {
+    noQuantiqueBarWidth = 1;
+    $("#no_quantique_span").hide();
+    setBarWidth("no_quantique", noQuantiqueBarWidth, 70);
+    quantiqueBarWidth = 1;
+    $("#quantique_span").hide();
+    setBarWidth("quantique", quantiqueBarWidth, 2000)
+});
+
+function setBarWidth(name, value, time) {
+    $("#"+name+"_bar").width(value+"%");
+    if (value != 100) {
+        setTimeout(() => {
+            value++;
+            setBarWidth(name, value, time);
+        }, time);
+    } else {
+        $("#"+name+"_span").show();
+    }
+}
